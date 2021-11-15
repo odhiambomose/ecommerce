09428616d9a5671b8e3a2e48688deeaecf5ee959
mosesodhiambo <modhiambo056@gmail.com>
2021-11-15 11:27:41 +0300

    created announcements components

---
+++ src/components/Announcements/Announcements.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Announcements.css"
+const Announcement=()=>{
+return(
+    <div className="bg-color">
+        <p className="offer">OFFER! Free Shipping for Products Above ksh. 30,000</p>
+    </div>
+)
+
+}
+export default Announcement;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Announcement from "../components/Announcements/Announcements";
+const Home=()=>{
+
+return(
+
+<Announcement/>
+
+)
+
+}
+export default Home;
