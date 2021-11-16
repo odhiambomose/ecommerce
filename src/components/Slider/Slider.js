79ea40b3d956a89ab3922f8e4ca65626f9ad6437
mosesodhiambo <modhiambo056@gmail.com>
2021-11-16 11:16:43 +0300

    created folder for images

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Slider.css"
+
+const Sliders=()=>{
+return(
+    <div>Sliders</div>
+)
+
+
+}
+
+export default Sliders
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Announcement from "../components/Announcements/Announcements";
 import Nav from "../components/Nav/Nav";
-
+import Slider from "../components/Slider/Slider"
 const Home=()=>{
 
 return(
 <div>
 <Announcement/>
 <Nav />
+<Slider />
 </div>
 
 )
