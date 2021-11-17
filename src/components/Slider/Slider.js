78ab5dc717597e993afdcdcad188d77a642fdd07
mosesodhiambo <modhiambo056@gmail.com>
2021-11-17 11:27:09 +0300

    created data for slides

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,9 +1,47 @@
 import React from "react";
 import "./Slider.css"
-
+import ArrowRightOutlinedIcon from '@material-ui/icons/ArrowRightOutlined';
+import ArrowLeftOutlinedIcon from '@material-ui/icons/ArrowLeftOutlined';
+import{slides} from "../../data"
+const handleClick=()=>{
+    alert("mose")
+}
 const Sliders=()=>{
 return(
-    <div>Sliders</div>
+    <div className="wrapper">
+        <div className="left-arrow" onClick={handleClick}>
+        <ArrowLeftOutlinedIcon />
+            
+        </div>
+<div className="slide">
+<div className="slide-image">
+    <img src="images/woman1.jpg" />
+</div>
+<div className="slide-content">
+    <h2>HOLIDAY OFFERS</h2>
+    <p>Grab our latest December offers</p>
+    <button>Buy Know</button>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+<div className="right-arrow" onClick={handleClick}>
+<ArrowRightOutlinedIcon />
+</div>
+
+    </div>
 )
 
 
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const slides=[
+    {
+        id:1,
+        img:"./images/woman.jpg",
+        title:"HOLIDAY OFFERS",
+        desc:"Grab our December Latest Offers"
+    },
+    {
+        id:2,
+        img:"./images/woman2.jpg",
+        title:"CLEARENCE SALE",
+        desc:"Get the best commodities in blink of an eye"
+
+
+    },
+    {
+        id:2,
+        img:"./images/woman3.jpg",
+        title:"NEW ARRIVAL",
+        desc:"Get new commodities at a cheap amount"
+
+    }
+]
