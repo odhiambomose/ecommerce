11a7900f08e6a2d137882644a120562a058263b3
mosesodhiambo <modhiambo056@gmail.com>
2021-12-07 13:14:01 +0300

    Updates

---
+++ precache-manifest.e7a85361d67123cfa492f1d714089be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864e09a3f04ff90673f006bf30d127dc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5163f748ca87de9d5523",
+    "url": "/my-app/static/css/main.bf78a52e.chunk.css"
+  },
+  {
+    "revision": "aafdbdb0b98c5aac7f24",
+    "url": "/my-app/static/js/2.96a4e805.chunk.js"
+  },
+  {
+    "revision": "befc7210e8d600b7724f6d6a2fb68035",
+    "url": "/my-app/static/js/2.96a4e805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5163f748ca87de9d5523",
+    "url": "/my-app/static/js/main.f4e95601.chunk.js"
+  },
+  {
+    "revision": "4e9a6009b76720099ac5",
+    "url": "/my-app/static/js/runtime-main.2b2b8dad.js"
+  }
+]);
